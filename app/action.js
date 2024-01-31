2d6bd0f3b6bd17506a9bd39be1f1a59be01d2467
pikriawan <80615517+pikriawan@users.noreply.github.com>
2024-01-31 23:34:15 +0000

    Update action.js

--- app/action.js
+++ app/action.js
@@ -48,14 +48,126 @@ export async function createProject(prevState, formData) {
     })
     revalidatePath('/')
     return {
-      message: 'Project creation successful',
-      data: {
-        project: {
-          ownerEmail: session?.user.email,
-          name: formData.get('name'),
-          data: ''
+      message: 'Project creation successful'
+    }
+  } catch (err) {
+    return {
+      error: 'Something went wrong'
+    }
+  }
+}
+
+export async function updateProject(
+  projectName,
+  prevState,
+  formData
+) {
+  const session = await auth()
+
+  if (!session?.user) {
+    return {
+      error: 'Not authenticated'
+    }
+  }
+
+  const updatedFields = {}
+
+  if (formData.get('name')) {
+    updatedFields.name = formData.get('name')
+  }
+
+  if (formData.get('data')) {
+    updatedFields.data = formData.get('data')
+  }
+
+  try {
+    await mongoClient.connect()
+    const projectCollection = mongoClient.db(process.env.DATABASE_NAME).collection('projects')
+    const existingProject = await projectCollection.findOne({
+      ownerEmail: session?.user.email,
+      name: projectName
+    })
+
+    if (!existingProject) {
+      return {
+        error: 'Project not found'
+      }
+    }
+
+    if (updatedFields.name) {
+      const projectSchema = z.object({
+        name: z.string().regex(/[ \-.0-9a-z]/gi).min(3).max(20)
+      })
+      const validatedFields = projectSchema.safeParse({
+        name: formData.get('name')
+      })
+
+      if (!validatedFields.success) {
+        return {
+          error: 'Invalid project name'
         }
       }
+
+      const isUpdatedProjectNameExist = await projectCollection.findOne({
+        ownerEmail: session?.user.email,
+        name: formData.get('name')
+      }) !== null
+
+      if (isUpdatedProjectNameExist) {
+        return {
+          error: 'Project name already taken'
+        }
+      }
+    }
+
+    await projectCollection.updateOne({
+      ownerEmail: session?.user.email,
+      name: projectName
+    }, {
+      $set: { ...updatedFields }
+    })
+    revalidatePath('/')
+    if (updatedFields.name) revalidatePath(`/project/${updatedFields.name}`)
+    return {
+      message: 'Project update successful'
+    }
+  } catch (err) {
+    return {
+      error: 'Something went wrong'
+    }
+  }
+}
+
+export async function deleteProject(projectName) {
+  const session = await auth()
+
+  if (!session?.user) {
+    return {
+      error: 'Not authenticated'
+    }
+  }
+
+  try {
+    await mongoClient.connect()
+    const projectCollection = mongoClient.db(process.env.DATABASE_NAME).collection('projects')
+    const existingProject = await projectCollection.findOne({
+      ownerEmail: session?.user.email,
+      name: projectName
+    })
+
+    if (!existingProject) {
+      return {
+        error: 'Project not found'
+      }
+    }
+
+    await projectCollection.deleteOne({
+      ownerEmail: session?.user.email,
+      name: projectName
+    })
+    revalidatePath('/')
+    return {
+      message: 'Project deletion successful'
     }
   } catch (err) {
     return {
