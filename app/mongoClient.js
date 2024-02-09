aad3ef2cb68996680ff9b5dac446c89a243921b9
pikriawan <80615517+pikriawan@users.noreply.github.com>
2024-02-09 00:20:10 +0000

    Add ESLint and fix some linter warnings

--- app/mongoClient.js
+++ app/mongoClient.js
@@ -1,9 +1,11 @@
 import { MongoClient, ServerApiVersion } from 'mongodb'
 
-export default new MongoClient(process.env.MONGODB_URI, {
+const mongoClient = new MongoClient(process.env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true
   }
 })
+
+export default mongoClient
