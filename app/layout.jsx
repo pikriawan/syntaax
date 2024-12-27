b57800cb9390bbd0608baf5ee9062fb8b11b4f34
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2024-12-27 19:40:10 +0700

    feat: add fonts

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,29 @@
+import { Inter, Poppins, Roboto_Mono } from "next/font/google";
+import "./globals.css";
 import { ErudaNoSSR } from "@/components/Eruda";
+
+export const inter = Inter({
+    subsets: ["latin"],
+    variable: "--font-inter",
+    display: "swap"
+});
+
+export const poppins = Poppins({
+    subsets: ["latin"],
+    weight: ["700"],
+    variable: "--font-poppins",
+    display: "swap"
+});
+
+export const roboto_mono = Roboto_Mono({
+    subsets: ["latin"],
+    variable: "--font-roboto-mono",
+    display: "swap"
+});
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
+        <html lang="en" className={`${inter.variable} ${poppins.variable} ${roboto_mono.variable}`}>
             <body>
                 {children}
                 {process.env.NODE_ENV === "development" && <ErudaNoSSR />}
