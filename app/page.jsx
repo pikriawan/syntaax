bab6ad042f5387addace58d14bffd03360c78946
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2024-12-30 23:15:23 +0700

    refactor: refactor navbar

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,12 @@
 import styles from "./styles.module.css";
 import Appbar from "@/components/Appbar";
-import MobileNavbar from "@/components/MobileNavbar";
+import Navbar from "@/components/Navbar";
 
 export default function HomePage() {
     return (
         <Appbar className={styles["app-bar"]}>
-            <MobileNavbar />
+            <Navbar />
+            <main className={styles.main} />
         </Appbar>
     );
 }
---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { Bars3Icon, DocumentIcon, UserIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { useState, useEffect } from "react";
+import { createPortal } from "react-dom";
+import styles from "./styles.module.css";
+import Appbar from "@/components/Appbar";
+import { BaseButton } from "@/components/Button";
+import Link from "@/components/Link";
+
+export default function Navbar() {
+    const pathname = usePathname();
+    const [mounted, setMounted] = useState(false);
+    const [show, setShow] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    return (
+        <>
+            <BaseButton className={styles["navbar-toggle"]} onClick={() => setShow(true)}>
+                <Bars3Icon className={styles.icon} />
+            </BaseButton>
+            {mounted && createPortal(
+                <div className={clsx(styles["navbar"], show && styles.show)}>
+                    <Appbar className={styles["app-bar"]}>
+                        <Link href="/" onClick={() => setShow(false)}>
+                            <Image
+                                width={16}
+                                height={16}
+                                src="/syntaax.svg"
+                                alt="Syntaax"
+                                className={styles.brand}
+                            />
+                        </Link>
+                        <BaseButton onClick={() => setShow(false)}>
+                            <XMarkIcon className={styles.icon} />
+                        </BaseButton>
+                    </Appbar>
+                    <div className={styles["navbar-nav"]}>
+                        <Link href="/" className={clsx(styles["navbar-item"], pathname === "/" && styles.active)} onClick={() => setShow(false)}>
+                            <DocumentIcon className={styles["navbar-item-icon"]} />
+                            <span className={styles["navbar-item-text"]}>Projects</span>
+                        </Link>
+                        <Link href="/profile" className={clsx(styles["navbar-item"], pathname === "/profile" && styles.active)} onClick={() => setShow(false)}>
+                            <UserIcon className={styles["navbar-item-icon"]} />
+                            <span className={styles["navbar-item-text"]}>Profile</span>
+                        </Link>
+                    </div>
+                </div>,
+                document.body
+            )}
+        </>
+    );
+}
