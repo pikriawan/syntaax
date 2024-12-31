8f94120ba25b320018df6ce6bd4d10b746bedb17
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2024-12-31 22:30:39 +0700

    refactor: update dashboard's appbar style

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -1,13 +1,17 @@
 "use client";
 
-import { Bars3Icon } from "@heroicons/react/24/outline";
+import { Bars3Icon, PlusIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 import styles from "./styles.module.css";
 import Appbar from "@/components/Appbar";
 import { BaseButton } from "@/components/Button";
+import { BaseLink } from "@/components/Link";
 import Navbar from "@/components/Navbar";
 
 export default function DashboardLayout({ children }) {
+    const pathname = usePathname();
     const [navbarShow, setNavbarShow] = useState(false);
 
     return (
@@ -16,6 +20,20 @@ export default function DashboardLayout({ children }) {
                 <BaseButton className={styles["navbar-toggle"]} onClick={() => setNavbarShow(true)}>
                     <Bars3Icon width={24} height={24} stroke="#F6F6F6" className={styles.icon} />
                 </BaseButton>
+                <BaseLink href="/" className={styles.brand}>
+                    <Image
+                        width={16}
+                        height={16}
+                        src="/syntaax.svg"
+                        alt="Syntaax"
+                    />
+                    <span>syntaax</span>
+                </BaseLink>
+                {pathname === "/" && (
+                    <BaseButton className={styles["create-project-form-toggle"]}>
+                        <PlusIcon width={24} height={24} stroke="#F6F6F6" className={styles.icon} />
+                    </BaseButton>
+                )}
             </Appbar>
             <Navbar className={styles.navbar} show={navbarShow} onHide={() => setNavbarShow(false)} />
             <main className={styles.main}>{children}</main>
