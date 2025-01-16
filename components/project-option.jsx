09a2cc39159e4998c35130bcb254a19d2e28f187
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2025-01-16 14:32:45 +0700

    feat: add option-modal and option-button

--- components/project-option.jsx
+++ components/project-option.jsx
@@ -3,8 +3,9 @@
 import { EllipsisHorizontalIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
 import DeleteProjectForm from "./delete-project-form";
-import EditProjectForm from "./edit-project-form"; 
-import Modal from "./ui/modal";
+import EditProjectForm from "./edit-project-form";
+import OptionButton from "./ui/option-button";
+import OptionModal from "./ui/option-modal";
 
 export default function ProjectOption({ publicId, name }) {
     const [open, setOpen] = useState(false);
@@ -16,14 +17,14 @@ export default function ProjectOption({ publicId, name }) {
             <button className="absolute top-4 right-4" onClick={() => setOpen(true)}>
                 <EllipsisHorizontalIcon className="w-6 h-6" />
             </button>
-            <Modal open={open} onClose={() => setOpen(false)} className="p-[0.0625rem] top-1/2 -translate-y-1/2">
+            <OptionModal open={open} onClose={() => setOpen(false)} className="p-[0.0625rem] top-1/2 -translate-y-1/2">
                 <div className="p-0 flex flex-col items-center">
-                    <button className="w-full py-2 rounded-t-[0.4375rem] hover:bg-zinc-900" onClick={() => {
+                    <OptionButton onClick={() => {
                         setOpen(false);
                         setEditOpen(true);
                     }}>
                         Edit
-                    </button>
+                    </OptionButton>
                     <EditProjectForm
                         open={editOpen}
                         onClose={() => setEditOpen(false)}
@@ -31,21 +32,21 @@ export default function ProjectOption({ publicId, name }) {
                         name={name}
                     />
                     <div className="w-full h-[0.0625rem] bg-zinc-800" />
-                    <button className="w-full py-2 text-red-500 hover:bg-zinc-900" onClick={() => {
+                    <OptionButton onClick={() => {
                         setOpen(false);
                         setDeleteOpen(true);
                     }}>
                         Delete
-                    </button>
+                    </OptionButton>
                     <DeleteProjectForm
                         open={deleteOpen}
                         onClose={() => setDeleteOpen(false)}
                         publicId={publicId}
                     />
                     <div className="w-full h-[0.0625rem] bg-zinc-800" />
-                    <button className="w-full py-2 hover:bg-zinc-900 rounded-b-[0.4375rem]" onClick={() => setOpen(false)}>Cancel</button>
+                    <OptionButton onClick={() => setOpen(false)}>Cancel</OptionButton>
                 </div>
-            </Modal>
+            </OptionModal>
         </>
     );
 }
