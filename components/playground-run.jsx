64e9104a90977a46b8bc380fea43475a55c07963
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2025-01-27 15:38:54 +0700

    feat: add keyboard shortcut hint on playground run button

--- components/playground-run.jsx
+++ components/playground-run.jsx
@@ -83,7 +83,7 @@ export default function PlaygroundRun() {
     }, [fetching, pushing, run]);
 
     return (
-        <button onClick={run} disabled={fetching || pushing} title="Run">
+        <button onClick={run} disabled={fetching || pushing} title="Run [Ctrl + Shift + R]">
             <PlayIcon className="w-6 h-6" />
         </button>
     );
