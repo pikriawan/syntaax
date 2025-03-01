003d569336fabc459d72df98bbaaaed06ad8ad8c
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2025-03-01 11:37:35 +0700

    chore: replace editor component on landing page with image

--- app/landing/page.jsx
+++ app/landing/page.jsx
@@ -1,9 +1,10 @@
 import { SiGithub, SiInstagram, SiX } from "@icons-pack/react-simple-icons";
-import { ChevronLeft, Ellipsis, Globe, Play, Smartphone, SquareArrowOutUpRight, Zap } from "lucide-react";
+import { Globe, Smartphone, Zap } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import SyntaaxIcon from "@/components/icons/syntaax-icon";
-import Button from "@/components/ui/button";
 import { getUser } from "@/data/user";
+import editor from "@/public/editor.png";
 
 export default async function LandingPage() {
     const user = await getUser();
@@ -27,76 +28,9 @@ export default async function LandingPage() {
                         <h1 className="font-bold text-5xl text-center">Instant HTML, CSS, JS playground</h1>
                         <Link href={user ? "/playgrounds" : "/signin"} className="bg-zinc-50 text-zinc-950 px-4 py-2 rounded-lg transition-colors hover:bg-zinc-400 disabled:bg-zinc-800 disabled:text-zinc-50 disabled:opacity-40 text-sm">Start creating</Link>
                     </div>
-                    <div className="w-full shadow-[0_0_0_0.0625rem_#27272A_inset] p-3 rounded-[1.25rem]">
-                        <div className="w-full shadow-[0_0_0_0.0625rem_#27272A_inset] rounded-lg">
-                            <div className="w-full aspect-[9/16] md:aspect-video flex flex-col">
-                                <div className="w-full h-14 px-4 flex items-center justify-between gap-4 shadow-[0_-0.0625rem_#27272A_inset]">
-                                    <div className="grid grid-cols-[auto_auto] items-center gap-4">
-                                        <ChevronLeft size={20} />
-                                        <h2 className="font-bold text-2xl truncate">My Playground</h2>
-                                    </div>
-                                    <div className="flex items-center gap-4">
-                                        <Play size={20} />
-                                        <SquareArrowOutUpRight size={20} />
-                                        <Ellipsis size={20} />
-                                    </div>
-                                </div>
-                                <div className="w-full h-[calc(100%-3.5rem)] md:grid md:grid-rows-1 md:grid-cols-[1fr_auto_1fr] p-[0_0.0625rem_0.0625rem_0.0625rem]">
-                                    <div className="grid grid-rows-[auto_1fr] grid-cols-1 h-full">
-                                        <div className="p-4 shadow-[0_-0.0625rem_#27272A_inset] w-full grid grid-cols-[repeat(3,1fr)] gap-4">
-                                            <Button className="hover:bg-zinc-50 cursor-auto">HTML</Button>
-                                            <Button className="hover:bg-zinc-950 cursor-auto" color="secondary">CSS</Button>
-                                            <Button className="hover:bg-zinc-950 cursor-auto" color="secondary">JS</Button>
-                                        </div>
-                                        <div className="w-full bg-zinc-950 text-[0.8125rem] whitespace-nowrap overflow-hidden rounded-[0_0_0_0.6875rem]">
-                                            <div className="flex">
-                                                <div className="flex flex-col items-end font-mono px-4 py-1">
-                                                    <span className="leading-[1.1375rem]">1</span>
-                                                    <span className="leading-[1.1375rem]">2</span>
-                                                    <span className="leading-[1.1375rem]">3</span>
-                                                    <span className="leading-[1.1375rem]">4</span>
-                                                    <span className="leading-[1.1375rem]">5</span>
-                                                    <span className="leading-[1.1375rem]">6</span>
-                                                    <span className="leading-[1.1375rem]">7</span>
-                                                    <span className="leading-[1.1375rem]">8</span>
-                                                    <span className="leading-[1.1375rem]">9</span>
-                                                    <span className="leading-[1.1375rem]">10</span>
-                                                    <span className="leading-[1.1375rem]">11</span>
-                                                    <span className="leading-[1.1375rem]">12</span>
-                                                    <span className="leading-[1.1375rem]">13</span>
-                                                    <span className="leading-[1.1375rem]">14</span>
-                                                    <span className="leading-[1.1375rem]">15</span>
-                                                </div>
-                                                <div className="flex flex-col font-mono pe-4 py-1">
-                                                    <span className="leading-[1.1375rem]">&lt;!DOCTYPE html&gt;</span>
-                                                    <span className="leading-[1.1375rem]">&lt;<span className="text-[#62C073]">html</span>&gt;</span>
-                                                    <span className="leading-[1.1375rem]">&nbsp;&nbsp;&nbsp;&nbsp;&lt;<span className="text-[#62C073]">head</span>&gt;</span>
-                                                    <span className="leading-[1.1375rem]">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;<span className="text-[#62C073]">meta</span>&nbsp;<span className="text-[#BF7AF0]">charset</span><span className="text-[#F75F8F]">=</span><span className="text-[#62C073]">&quot;UTF-8&quot;</span>&gt;</span>
-                                                    <span className="leading-[1.1375rem]">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;<span className="text-[#62C073]">meta</span>&nbsp;<span className="text-[#BF7AF0]">name</span><span className="text-[#F75F8F]">=</span><span className="text-[#62C073]">&quot;viewport&quot;</span>&nbsp;<span className="text-[#BF7AF0]">content</span><span className="text-[#F75F8F]">=</span><span className="text-[#62C073]">&quot;width=device-width,&nbsp;initial-scale=1&quot;</span>&gt;</span>
-                                                    <span className="leading-[1.1375rem]">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;<span className="text-[#62C073]">title</span>&gt;My Playground&lt;/<span className="text-[#62C073]">title</span>&gt;</span>
-                                                    <span className="leading-[1.1375rem]">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;<span className="text-[#62C073]">link</span>&nbsp;<span className="text-[#BF7AF0]">rel</span><span className="text-[#F75F8F]">=</span><span className="text-[#62C073]">&quot;stylesheet&quot;</span>&nbsp;<span className="text-[#BF7AF0]">href</span><span className="text-[#F75F8F]">=</span><span className="text-[#62C073]">&quot;style.css&quot;</span>&gt;</span>
-                                                    <span className="leading-[1.1375rem]">&nbsp;&nbsp;&nbsp;&nbsp;&lt;/<span className="text-[#62C073]">head</span>&gt;</span>
-                                                    <span className="leading-[1.1375rem]">&nbsp;&nbsp;&nbsp;&nbsp;&lt;<span className="text-[#62C073]">body</span>&gt;</span>
-                                                    <span className="leading-[1.1375rem]">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;<span className="text-[#62C073]">h2</span>&gt;Hello!&lt;/<span className="text-[#62C073]">h2</span>&gt;</span>
-                                                    <span className="leading-[1.1375rem]">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;<span className="text-[#62C073]">p</span>&gt;Welcome to my playground!&lt;/<span className="text-[#62C073]">p</span>&gt;</span>
-                                                    <span className="leading-[1.1375rem]">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;<span className="text-[#62C073]">button</span>&gt;Click me!&lt;/<span className="text-[#62C073]">button</span>&gt;</span>
-                                                    <span className="leading-[1.1375rem]">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;<span className="text-[#62C073]">script</span>&nbsp;<span className="text-[#BF7AF0]">src</span><span className="text-[#F75F8F]">=</span><span className="text-[#62C073]">&quot;script.js&quot;</span>&gt;&lt;/<span className="text-[#62C073]">script</span>&gt;</span>
-                                                    <span className="leading-[1.1375rem]">&nbsp;&nbsp;&nbsp;&nbsp;&lt;/<span className="text-[#62C073]">body</span>&gt;</span>
-                                                    <span className="leading-[1.1375rem]">&lt;/<span className="text-[#62C073]">html</span>&gt;</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="w-[0.0625rem] h-full hidden bg-zinc-800 md:block" />
-                                    <div className="h-full hidden bg-zinc-950 md:block rounded-[0_0_0.6875rem_0]">
-                                        <div className="p-4">
-                                            <h2 className="text-3xl font-bold">Hello!</h2>
-                                            <p className="mt-2">Welcome to my playground!</p>
-                                            <Button className="mt-4 hover:bg-zinc-50 cursor-auto">Click me!</Button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                    <div className="w-full shadow-[0_0_0_0.0625rem_#27272A_inset] p-2 md:p-3 rounded-[calc(0.5rem+0.5rem+0.0625rem)] md:rounded-[calc(0.75rem+0.5rem+0.0625rem)]">
+                        <div className="w-full shadow-[0_0_0_0.0625rem_#27272A_inset] rounded-[calc(0.5rem+0.0625rem)] p-[0.0625rem]">
+                            <Image alt="Editor" className="rounded-lg" src={editor} />
                         </div>
                     </div>
                 </section>
