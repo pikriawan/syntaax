36eb8f666bcdddeeb4bdccb44b567ca3ae9f6b6f
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-02-19 20:15:17 +0700

    feat: create database migration script

---
+++ db/migrate.js
@@ -0,0 +1,29 @@
+const { neon } = require("@neondatabase/serverless");
+require("dotenv").config();
+
+const sql = neon(process.env.DATABASE_URL);
+
+async function migrate() {
+    await sql`
+        CREATE TABLE IF NOT EXISTS users(
+            id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+            name VARCHAR NOT NULL,
+            email VARCHAR UNIQUE NOT NULL,
+            image TEXT
+        );
+    `;
+    
+    await sql`
+        CREATE TABLE IF NOT EXISTS playgrounds(
+            id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+            user_id UUID NOT NULL REFERENCES users(id),
+            name VARCHAR NOT NULL,
+            created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+        );
+    `;
+
+    console.log("Migration successful");
+}
+
+migrate();
